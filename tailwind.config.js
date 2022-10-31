369134a5619e8358a2352a7136ef79922fe56457
Hugo Averty <hugoaverty@gmail.com>
2022-10-31 08:45:07 +0100

    Adding info on global settings and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -246,6 +246,9 @@ module.exports = {
     boxShadow: {
       sm: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
       DEFAULT: '0 0 15px 0 rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
+      bxShadow: '0 10px 20px 5px rgba(0,0,0,0.35)',
+      bxShadowHover: '0 5px 10px 2px rgba(0,0,0,0.50)',
+      bxShadowSmall: '0 3px 5px 1px rgba(0,0,0,0.75)',
       md: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
       lg: '0 0 10px 0 rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
       xl: '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
